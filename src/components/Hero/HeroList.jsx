0091f3402a936a4932c2b1614bfc8d381662b2c9
julskdj <julio22567@hotmail.com>
2022-04-12 16:50:00 -0500

    commit5

--- src/components/Hero/HeroList.jsx
+++ src/components/Hero/HeroList.jsx
@@ -6,11 +6,11 @@ const HeroList = ({ publisher }) => {
   const heroes = useMemo(()=> getHeroByPublisher(publisher), [publisher]) ;
 
   return (
-    <div className="row rows-cols-1 row-cols-md-3 g-3">
+    <div className="row rows-cols-1 row-cols-md-3 g-3 animate__animated animate__fadeIn">
       {heroes.map((heroe, index) => (
-        <HeroCard key={heroe.id} {...heroe}>
-          {heroe.superhero}
-        </HeroCard>
+        <HeroCard key={heroe.id} {...heroe}/>
+          
+        
       ))}
     </div>
   );
