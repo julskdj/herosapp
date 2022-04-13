e9c33cdda0242a2dd8fed01cb61f957f8ed2a109
julskdj <julio22567@hotmail.com>
2022-04-13 14:45:23 -0500

    antes de irme

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/authContext";
 
 export const Navbar = () => {
   const navigate = useNavigate();
+
+  const {user} = useContext(AuthContext)
+
+  const {name} = user
 
   const handleLogout = () => {
     //TODOs Logout
@@ -53,7 +58,7 @@ export const Navbar = () => {
 
         <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
           <ul className="navbar-nav ml-auto">
-            <span className="nav-item nav-link text-info">Julio</span>
+            <span className="nav-item nav-link text-info">{name}</span>
 
             <button className="nav-item nav-link btn" onClick={handleLogout}>
               Logout
