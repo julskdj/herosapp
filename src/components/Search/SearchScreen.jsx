d27c51bbb825c17d1dd5331a6ddb780535e5300b
julskdj <julio22567@hotmail.com>
2022-04-13 08:38:02 -0500

    commit5

--- src/components/Search/SearchScreen.jsx
+++ src/components/Search/SearchScreen.jsx
@@ -1,10 +1,42 @@
+import { useForm } from "../../hooks/useForm";
+
 const SearchScreen = () => {
-    return (
-      <div>
-        <h1>SearchScreen</h1>
-      </div>
-    );
+  const [{ searchText }, handleInputChange] = useForm({
+    searchText: "",
+  });
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(searchText);
   };
-  
-  export default SearchScreen;
-  
+
+  return (
+    <>
+      <h1>SearchScreen</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Formilario de Busqueda</h4>
+          <hr />
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="Buscar un Heroe"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              onChange={handleInputChange}
+              value={searchText}
+            />
+
+            <button type="submit" className="btn btn-outline-primary mt-2">
+              Buscar
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SearchScreen;
